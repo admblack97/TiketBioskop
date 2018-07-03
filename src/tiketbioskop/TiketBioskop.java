91bf9d5f76eab69bb2c6c4c3f1334b701ff7c509
admblack97@gmail.com <admblack97@gmail.com>
2018-07-04 06:54:07 +0700

    switch

--- src/tiketbioskop/TiketBioskop.java
+++ src/tiketbioskop/TiketBioskop.java
@@ -18,8 +18,58 @@ public class TiketBioskop {
             System.out.println("|Studio 8. Ant-Man And The Wasp|");
             System.out.println("|9. Exit");
             System.out.println("|==============================|");
-            System.out.print("Pilihan [1/2/3/4/5/6/7/8/9] = ");
+            System.out.print("Choose one [1/2/3/4/5/6/7/8/9] = ");
             int pilih =input.nextInt();
+            
+            switch (pilih){
+                case 1:{
+                    Studio1 s1=new Studio1();
+                    s1.Studio1();
+                    break;
+                }
+                case 2:{
+                    Studio2 s2=new Studio2();
+                    s2.Studio2();
+                    break;
+                }
+                case 3:{
+                   Studio3 s3=new Studio3();
+                   s3.Studio3();
+                   break;
+                }
+                case 4:{
+                   Studio4 s4=new Studio4();
+                   s4.Studio4();
+                   break;
+                }
+                case 5:{
+                   Studio5 s5=new Studio5();
+                   s5.Studio5();
+                   break;
+                }
+                case 6:{
+                   Studio6 s6=new Studio6();
+                   s6.Studio6();
+                   break;
+                }
+                case 7:{
+                   Studio7 s7=new Studio7();
+                   s7.Studio7();
+                   break;
+                }
+                case 8:{
+                   Studio8 s8=new Studio8();
+                   s8.Studio8();
+                   break;
+                }
+                case 9:{
+                   System.exit(9);
+                }
+                break;
+            }
+            System.out.println("|==============================|");
+            System.out.print("Do you want to order again [y/n]? ");
+            ulg=input.next();
         }
     }
     
